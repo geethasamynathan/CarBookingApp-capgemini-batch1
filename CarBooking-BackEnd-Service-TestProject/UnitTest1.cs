7ea65a8c4a92b9d81cd28a2131d7c9d8e7df078a
geethasamynathan <geethasamynathan2011@gmail.com>
2024-10-12 15:30:05 +0530

    Add project files.

---
+++ CarBooking-BackEnd-Service-TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CarBooking_BackEnd_Service_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
