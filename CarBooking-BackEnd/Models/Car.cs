3623eaed7e23694c1583a096cbd3a9d46aaf6e22
SHIRISHA-NOMULA <shirishareddynomula@gmail.com>
2024-10-16 10:14:35 +0530

    Added UserManagement,Car,Review classes

---
+++ CarBooking-BackEnd/Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarBooking_BackEnd.Models
+{
+    public class Car
+
+    {
+        [Key]
+        public int CarID { get; set; }
+
+        [Required(ErrorMessage = "Car name is required.")]
+        [StringLength(100, ErrorMessage = "Car name cannot exceed 100 characters.")]
+        public string CarName { get; set; }
+
+        //[Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Stock quantity is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity cannot be negative.")]
+        public int StockQuantity { get; set; }
+
+        public string ImageURL { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be between 0 and 5.")]
+        public double Rating { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+}
